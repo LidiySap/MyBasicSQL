b218567569658b36c20430798f9ded3d65582f35
LidiySap <90103154+LidiySap@users.noreply.github.com>
2021-09-13 14:41:07 +0300

    Создано представление vw_goods

--- Views.sql
+++ Views.sql
@@ -1,1 +1,7 @@
-
+# Создание представления vw_goods, включающие в себя только необходимые ключевые поля и названия сущностей
+CREATE VIEW vw_goods AS 
+  SELECT g.id_goods, g.goods, g.price, 
+    c.id_categories, c.categories_name 
+  FROM goods AS g 
+    INNER JOIN goods_categories AS c 
+      ON g.id_categories = c.id_categories; 
