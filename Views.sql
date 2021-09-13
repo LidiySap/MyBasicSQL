3f53bd7556651e8b7f1d4464c4fa32900529fb68
LidiySap <90103154+LidiySap@users.noreply.github.com>
2021-09-13 14:42:31 +0300

    Создан SQL запрос к представлению vw_goods

--- Views.sql
+++ Views.sql
@@ -5,3 +5,14 @@ CREATE VIEW vw_goods AS
   FROM goods AS g 
     INNER JOIN goods_categories AS c 
       ON g.id_categories = c.id_categories; 
+
+## Создание SQL запроса, вычисляющего количество товаров в каждой категории, используя внутреннее и внешнее объединения
+SELECT c.categories_name AS `Категория`, sub.cnt AS `Количество товаров` 
+  FROM goods_categories AS c 
+    LEFT JOIN 
+(SELECT id_categories, COUNT(goods) AS cnt 
+  FROM vw_goods 
+    GROUP BY id_categories 
+) AS sub 
+  ON c.id_categories = sub.id_categories 
+    ORDER BY categories_name; 
